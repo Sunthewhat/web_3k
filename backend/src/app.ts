e76045b0ea69082ea9795266bc7b8975546cb9c5
Sunthewhat <chotiwet.wisi@icloud.com>
2025-01-14 13:59:32 +0700

    fix/cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors(corsConfig()));
+app.use(
+	cors({
+		origin: '*',
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(addressTracker);
